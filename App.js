041f876c910cb777f84555f5b44d4d53e3cfecb0
HugoBaviolet <hbaviolet@hotmail.co.uk>
2021-03-17 09:43:52 +0000

    cont

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+
+import Header from './components/Header';
+import StartGame from './screens/StartGame';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View stlye={styles.screen}>
+      <Header title="Guess a number"/>
+
+      <StartGame />
+
     </View>
-  );
-}
+  )
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  screen:{
+    
+  }
+  });
